d1ecc7a05d1f97ce913b2d249d7584c9fa5ac12e
神楽坂覚々 <komeiji@satori.moe>
2018-04-06 03:55:19 +0800

    add Bank Transfer Notify

--- application/controllers/Bank.php
+++ application/controllers/Bank.php
@@ -35,7 +35,6 @@ class Bank extends CI_Controller {
 				'code' => 404,
 				'msg' => 'user.not_exists',
 			];
-
 		}
 		$self_balance = $this->db->select("username,balance")->get_where("Balance", ["username" => $name])->result()[0]->balance;
 		$to_balance = $this->db->select("username,balance")->get_where("Balance", ["username" => $to_name])->result()[0]->balance;
@@ -57,6 +56,9 @@ class Bank extends CI_Controller {
 		$this->db->update("Balance", $self_transfer);
 		$this->db->where('username', $to_name);
 		$this->db->update("Balance", $to_transfer);
+
+		// $this->utils->sendMessage($name, '§a[银行] §f 您向 %s 转账了 %s 个 NekoCoin');
+		// $this->utils->sendMessage($to_name, '§a[银行] §f %s 向您转账了 %s 个 NekoCoin');
 		return [
 			'code' => 200,
 			'message' => 'user.transfer.success',
